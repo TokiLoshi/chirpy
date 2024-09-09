ba0ac0b70ed3eca80fd0e43fb76b7f63421ddf9f
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2024-09-09 13:34:23 -0700

    setting up mux

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+type Server struct{}
+
+func (Server) ServeHTTP(http.ResponseWriter, *http.Request) {}
+
 func main() {
-	fmt.Println("Hello world")
+
+	// 1. Create a new http.ServeMux
+	mux := http.NewServeMux()
+	mux.Handle("/localhost", Server{})
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		// The "/" pattern matches everything, so we need to check 
+		// that we're at the root here 
+		if req.URL.Path != "/ok" {
+			http.NotFound(w, req)
+			return
+		}
+		fmt.Fprintf(w, "Home Page")
+	})
+
+	http.ListenAndServe(":8080", mux)
+
 }
