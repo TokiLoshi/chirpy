b6e910b6ac948a1f85637de11a6d33db91e6a90e
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-02-18 12:52:38 -0800

    serving images

--- main.go
+++ main.go
@@ -9,18 +9,20 @@ import (
 
 func main() {
 	const port  = "8080"
+	const filePathRoute = "."
 	log.Println("hello world, starting server")
 
 	
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./")))
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoute)))
+	mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
 
 	server := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
 	}
-	log.Printf("Serving on port %s\n", port)
+	log.Printf("Serving files from %s on port %s\n", filePathRoute, port)
 	log.Fatal(server.ListenAndServe())
 
 	
