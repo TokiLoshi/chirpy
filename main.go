4a2eb277bb657d9da138a832709d5aca9e98b380
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-02-18 14:01:27 -0800

    adding custom handlers

--- main.go
+++ main.go
@@ -6,17 +6,20 @@ import (
 )
 
 
-
 func main() {
 	const port  = "8080"
 	const filePathRoute = "."
 	log.Println("hello world, starting server")
 
-	
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoute)))
-	mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoute))))
+	mux.Handle("/assets/", http.FileServer(http.Dir("./assets")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))	
+	})
 
 	server := &http.Server{
 		Addr: ":" + port,
