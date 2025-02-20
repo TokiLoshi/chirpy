bfbb0f8c667a34fa6d264798e88f4871ff406b24
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-02-19 16:35:17 -0800

    adding routing and middleware

--- main.go
+++ main.go
@@ -1,25 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func(cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func(cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
+		hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
 
 
 func main() {
 	const port  = "8080"
 	const filePathRoute = "."
+	apiCfg := &apiConfig{}
 	log.Println("hello world, starting server")
 
+	
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoute))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoute)))))
 	mux.Handle("/assets/", http.FileServer(http.Dir("./assets")))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))	
 	})
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)	
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
 
 	server := &http.Server{
 		Addr: ":" + port,
