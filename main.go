a8431198ddacb719f5dcd9782dc72e91ee3b2197
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-02-21 12:44:07 -0800

    adding json response handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -37,6 +38,68 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
 
 }
 
+func validateChirps(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	// Decode the request 
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	
+	// Handle errors decoding the request 
+	err := decoder.Decode(&params)
+	if err != nil {
+		decodeError := errorResponse{
+			Error : "error decoding json data",
+		}
+		data, error := json.Marshal(decodeError)
+		if error != nil {
+			log.Printf("error marshalling JSON %s", err)
+		}
+		log.Printf("Error decoding parameters %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(data)
+		return 
+	}
+
+	// Check if chirps length is too long 
+	if len(params.Body) > 140 {
+		errorRespBody := errorResponse{
+			Error: "Chirp is too long",
+		}
+		data, err := json.Marshal(errorRespBody)
+		if err != nil {
+			log.Printf("error marshalling JSON: %s", err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(data)
+		return 
+	}
+	type correctResponse struct {
+		Valid bool `json:"valid"`
+	}
+	respBody := correctResponse{
+		Valid : true,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("error marshalling JSON: %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)	
+	w.Write(dat)
+
+}
+
 
 func main() {
 	const port  = "8080"
@@ -59,7 +122,9 @@ func main() {
 	// Static file serving
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoute)))))
 	mux.Handle("/assets/", http.FileServer(http.Dir("./assets")))
-
+	
+	// API endpoints 
+	mux.HandleFunc("POST /api/validate_chirp", validateChirps)
 
 	server := &http.Server{
 		Addr: ":" + port,
